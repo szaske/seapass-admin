8b5898f614d11217ef2fb58f38bcc67d81bccf37
Steve Zaske <github@zaske.com>
2017-10-10 16:49:49 -0700

    Add ability to Update and existing POI

--- src/app/poi-detail/poi-detail.component.ts
+++ src/app/poi-detail/poi-detail.component.ts
@@ -12,30 +12,60 @@ import { PoiService } from '../poi.service';
 })
 export class PoiDetailComponent implements OnInit {
   poiId: string;
-  
-  poi;
+  poi; // A single POI
+  poiDBRef; // Reference to the specific document in FireStore
 
-  editFormVisable: boolean = false;
-
-  constructor(private route: ActivatedRoute, private router: Router, private poiService: PoiService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private poiService: PoiService) { 
+    
+  }
 
   ngOnInit() {
+    // We need to instantiate poi or we'll get errors on HTML load 
+    this.poi = {};
+    
     this.route.params.forEach((urlParameters) => {
       this.poiId = urlParameters['autoId'];
     });
 
+    // Kept for Update purposes
+    this.poiDBRef = this.poiService.getPoiObsByAutoId(this.poiId);
+
     // This fetches a Poi from FB, and turns the observable into an actual poi
-    this.poiService.getPoiByAutoId(this.poiId).subscribe(emitData => {
+    this.poiDBRef.subscribe(emitData => {
      this.poi = new Poi(
        emitData.name,
        emitData.description,
        emitData.latitude,
        emitData.longitude,
        emitData.img_url,
-        emitData.tags,
-      emitData.autoId);
-     console.log(this.poi);
+       emitData.tags,
+       emitData.autoId);
      });
-  }
 
+  } //end of ngOnInit
+
+  UpdatePOI(
+    editName: string, 
+    editDesc: string, 
+    editLatitude: number, 
+    editLongitude: number, 
+    editImgUrl: string, 
+    editTags: string, 
+    editAutoId: string) {
+
+      console.log("STZ: update event occured");
+
+      // Split tags into an array
+      var tagsArray = editTags.split(",");
+
+      var updatedPOI: Poi = new Poi(
+        editName,
+        editDesc,
+        editLatitude,
+        editLongitude,
+        editImgUrl,
+        tagsArray,
+        editAutoId);
+    this.poiService.updatePoi(updatedPOI);   
+  }
 }
--- src/app/poi.service.ts
+++ src/app/poi.service.ts
@@ -31,12 +31,29 @@ export class PoiService {
   }
 
   // Returns an observable
-  getPoiByAutoId(poiId: string) {
+  getPoiObsByAutoId(poiId: string) {
     return this.poiCollection.doc(poiId).snapshotChanges().map(item => {
       const data = item.payload.data() as Poi;
       const autoId = item.payload.id;
       return {autoId, ...data}
     })
   } 
+    // public name: string, 
+  // public description:string,
+  // public latitude: number, 
+  // public longitude: number,
+  // public img_url?: string,
+  // public tags?: string[],
+  // public autoId?: string
+
+  updatePoi(localUpdatedPoi: Poi){
+    // var poiEntryInFirebase = this.getPoiObsByAutoId(localUpdatedPoi.autoId);
+    this.poiCollection.doc(localUpdatedPoi.autoId).update({name: localUpdatedPoi.name,
+      description: localUpdatedPoi.description,
+      latitude: localUpdatedPoi.latitude,
+      longitude: localUpdatedPoi.longitude,
+      img_url: localUpdatedPoi.img_url,
+      tags: localUpdatedPoi.tags});
+  }
 
 }
